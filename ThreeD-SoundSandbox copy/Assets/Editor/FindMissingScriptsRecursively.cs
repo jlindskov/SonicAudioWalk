67eace29dc657c0321f8cf65bf3569644c3e322e
Jeppe <jeppelind@icloud.com>
2022-03-28 21:42:50 +0200

    Removed missing scripts

--- ThreeD-SoundSandbox copy/Assets/Editor/FindMissingScriptsRecursively.cs
+++ ThreeD-SoundSandbox copy/Assets/Editor/FindMissingScriptsRecursively.cs
@@ -1,130 +1,63 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-namespace AndroidUltimatePlugin.Helpers.Editor
-{
-    public class FindMissingScriptsRecursively : EditorWindow
-    {
-        static int _goCount = 0, _componentsCount = 0, _missingCount = 0;
-
-        [MenuItem("Window/FindMissingScriptsRecursively")]
-        public static void ShowWindow()
-        {
-            GetWindow(typeof(FindMissingScriptsRecursively));
-        }
-
-        public void OnGUI()
-        {
-            if (GUILayout.Button("Find Missing Scripts in selected GameObjects"))
-            {
-                FindInSelected();
-            }
-
-            if (GUILayout.Button("Find Missing Scripts"))
-            {
-                FindAll();
-            }
-            EditorGUILayout.BeginHorizontal();
-            {
-                EditorGUILayout.LabelField("Component Scanned:");
-                EditorGUILayout.LabelField("" + (_componentsCount == -1 ? "---" : _componentsCount.ToString()));
-            }
-            EditorGUILayout.EndHorizontal();
-            
-            EditorGUILayout.BeginHorizontal();
-            {
-                EditorGUILayout.LabelField("Object Scanned:");
-                EditorGUILayout.LabelField("" + (_goCount == -1 ? "---" : _goCount.ToString()));
-            }
-            EditorGUILayout.EndHorizontal();
-            
-            EditorGUILayout.BeginHorizontal();
-            {
-                EditorGUILayout.LabelField("Possible Missing Scripts:");
-                EditorGUILayout.LabelField("" + (_missingCount == -1 ? "---" : _missingCount.ToString()));
-            }
-            EditorGUILayout.EndHorizontal();
-        }
-
-        private static void FindAll()
-        {
-            _componentsCount = 0;
-            _goCount = 0;
-            _missingCount = 0;
-            
-            string[] assetsPaths = AssetDatabase.GetAllAssetPaths();
-
-            foreach (string assetPath in assetsPaths)
-            {
-                Object[] data = LoadAllAssetsAtPath(assetPath);
-                foreach (Object o in data)
-                {
-                    if (o != null)
-                    {
-                        if (o is GameObject)
-                        {
-                            FindInGO((GameObject) o);
-                        }
-                    }
-                }
-            }
-            
-            Debug.Log($"Searched {_goCount} GameObjects, {_componentsCount} components, found {_missingCount} missing");
-        }
-
-        public static Object[] LoadAllAssetsAtPath(string assetPath)
-        {
-            return typeof(SceneAsset).Equals(AssetDatabase.GetMainAssetTypeAtPath(assetPath))
-                ?
-                // prevent error "Do not use readobjectthreaded on scene objects!"
-                new[] {AssetDatabase.LoadMainAssetAtPath(assetPath)}
-                : AssetDatabase.LoadAllAssetsAtPath(assetPath);
-        }
-
-        private static void FindInSelected()
-        {
-            GameObject[] go = Selection.gameObjects;
-            _goCount = 0;
-            _componentsCount = 0;
-            _missingCount = 0;
-            foreach (GameObject g in go)
-            {
-
-                FindInGO(g);
-            }
-
-            Debug.Log($"Searched {_goCount} GameObjects, {_componentsCount} components, found {_missingCount} missing");
-        }
-
-        private static void FindInGO(GameObject g)
-        {
-            _goCount++;
-            Component[] components = g.GetComponents<Component>();
-            for (int i = 0; i < components.Length; i++)
-            {
-                _componentsCount++;
-                if (components[i] == null)
-                {
-                    _missingCount++;
-                    string s = g.name;
-                    Transform t = g.transform;
-                    while (t.parent != null)
-                    {
-                        var parent = t.parent;
-                        s = parent.name + "/" + s;
-                        t = parent;
-                    }
-
-                    Debug.Log(s + " has an empty script attached in position: " + i, g);
-                }
-            }
-
-            // Now recurse through each child GO (if there are any):
-            foreach (Transform childT in g.transform)
-            {
-                //Debug.Log("Searching " + childT.name  + " " );
-                FindInGO(childT.gameObject);
-            }
-        }
-    }
-}
+﻿ using System.Collections.Generic;
+ using System.Linq;
+ using UnityEngine;
+ using UnityEditor;
+ public static class FindMissingScriptsRecursively
+ {
+     [MenuItem("Auto/Remove Missing Scripts Recursively Visit Prefabs")]
+     private static void FindAndRemoveMissingInSelected()
+     {
+         // EditorUtility.CollectDeepHierarchy does not include inactive children
+         var deeperSelection = Selection.gameObjects.SelectMany(go => go.GetComponentsInChildren<Transform>(true))
+             .Select(t => t.gameObject);
+         var prefabs = new HashSet<Object>();
+         int compCount = 0;
+         int goCount = 0;
+         foreach (var go in deeperSelection)
+         {
+             int count = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(go);
+             if (count > 0)
+             {
+                 if (PrefabUtility.IsPartOfAnyPrefab(go))
+                 {
+                     RecursivePrefabSource(go, prefabs, ref compCount, ref goCount);
+                     count = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(go);
+                     // if count == 0 the missing scripts has been removed from prefabs
+                     if (count == 0)
+                         continue;
+                     // if not the missing scripts must be prefab overrides on this instance
+                 }
+ 
+                 Undo.RegisterCompleteObjectUndo(go, "Remove missing scripts");
+                 GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+                 compCount += count;
+                 goCount++;
+             }
+         }
+ 
+         Debug.Log($"Found and removed {compCount} missing scripts from {goCount} GameObjects");
+     }
+ 
+     // Prefabs can both be nested or variants, so best way to clean all is to go through them all
+     // rather than jumping straight to the original prefab source.
+     private static void RecursivePrefabSource(GameObject instance, HashSet<Object> prefabs, ref int compCount,
+         ref int goCount)
+     {
+         var source = PrefabUtility.GetCorrespondingObjectFromSource(instance);
+         // Only visit if source is valid, and hasn't been visited before
+         if (source == null || !prefabs.Add(source))
+             return;
+ 
+         // go deep before removing, to differantiate local overrides from missing in source
+         RecursivePrefabSource(source, prefabs, ref compCount, ref goCount);
+ 
+         int count = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(source);
+         if (count > 0)
+         {
+             Undo.RegisterCompleteObjectUndo(source, "Remove missing scripts");
+             GameObjectUtility.RemoveMonoBehavioursWithMissingScript(source);
+             compCount += count;
+             goCount++;
+         }
+     }
+ }
