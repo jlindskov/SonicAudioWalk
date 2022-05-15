e97b791c19ce6f4995b6f24a943653e717950f88
Jeppe Lindskov <jeppelind@icloud.com>
2022-05-15 18:11:30 +0200

    Made it work on mac

--- 3DSoundSandbox/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ 3DSoundSandbox/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID TESTSINE_PLAY = 3923313419U;
+        static const AkUniqueID TESTSINE_PLAY_01 = 3807434623U;
     } // namespace EVENTS
 
     namespace BANKS
